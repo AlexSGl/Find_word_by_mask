174333e6df43f776396ace9b8c4317bacf50863c
User <alexandrglukhov@yandex.ru>
2023-09-22 17:43:25 +0300

    Commit2

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ my_file_word = open('russian_nouns.txt', "r", encoding='utf-8')
 data_read = my_file_word.read()
 list_noun = data_read.replace("\n", " ").split(" ")
 
-mask = input('Легенда: * - любая буква, g - любая гласная, s - любая согласная, а-я - точное соответствие.\nВведите маску:')
-
+mask = input(
+    'Легенда: * - любая буква, g - любая гласная, s - любая согласная, а-я - точное соответствие.\nВведите маску:')
 
 # Создание списка слов длиной = длине маски
 list_noun_len = []
@@ -24,11 +24,26 @@ for word in list_noun_len:
         else:
             word_new = []
             break
-    if word_new != []:
+    if word_new:
         list_noun_len_word.append(''.join(word_new))
         count += 1
 
 print('Найдено', count, 'слов:', list_noun_len_word)
 
+# Поиск дополнительных букв в слове
+char_str = input('Какие буквы есть в слове, введите через пробел:')
+char_arr = char_str.split()
+list_noun_len_word_add = []
+for word in list_noun_len_word:
+    count = 0
+    for char in char_arr:
+        if char in word:
+            count += 1
+            if count == len(char_arr):
+                list_noun_len_word_add.append(word)
+if len(list_noun_len_word_add):
+    print('Найдено', len(list_noun_len_word_add), 'слов:', list_noun_len_word_add)
+else:
+    print(f'Слова с буквами "{char_str}" отсутствуют')
 
 my_file_word.close()
